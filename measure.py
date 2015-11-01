e053dd62af64805b1f5227c6cf93b2b0c5638483
Didier Walliang <didier.walliang@gmail.com>
2015-11-01 22:10:02 +0100

    Add the Slice class (represent a slice of a star trail) Make a slice along a star trail and show the measurements of FWHM.

--- measure.py
+++ measure.py
@@ -7,6 +7,59 @@ import matplotlib.pyplot as plt
 from astropy.modeling import models, fitting
 import numpy as np
 
+# class definitions
+
+class Slice():
+    """Slice of a star trail"""
+
+    def __init__(self, positions, intensities):
+        self.positions = positions
+        self.intensities = intensities
+
+        # move the y data to the axis to allow the fitting
+        # (because the fitting doesn't work if the data are not near the x axis)
+        minIntensity = min(self.intensities)
+        self.relative_intensities = self.intensities - minIntensity
+
+    def print_positions(self):
+        print "positions = %s " % self.positions
+
+    def print_intensities(self):
+        print "intensities = %s " % self.intensities
+
+    def print_relative_intensities(self):
+        print "relative intensities = %s " % self.relative_intensities
+
+    def fit_gaussian(self):
+        """Fit the data using a Gaussian"""
+        max_relative_intensity = max(self.relative_intensities)
+        mean_position = sum(self.positions)/len(self.positions)
+
+        gaussian_init = models.Gaussian1D(amplitude=max_relative_intensity, mean=mean_position, stddev=3)
+        fit_gaussian = fitting.LevMarLSQFitter()
+        self.gaussian = fit_gaussian(gaussian_init, self.positions, self.relative_intensities)
+
+    def fwhm_from_gaussian(self):
+        """FWHM calculation with the best-fit model"""
+        sigma = self.gaussian.stddev.value
+        self.fwhmInPx = 2 * np.sqrt(2 * np.log(2)) * sigma
+
+    def print_graph(self):
+        # 200 dots from first to last position to have a smooth bell curve
+        firstPosition = self.positions[0]
+        lastPosition = self.positions[len(x_positions)-1]
+        finerPositions = np.linspace(firstPosition, lastPosition, 200)
+
+        plt.figure(1)
+        plt.plot(self.positions, self.relative_intensities, 'ko')
+        plt.plot(finerPositions, self.gaussian(finerPositions))
+        plt.xlabel('Position (pixel)')
+        plt.ylabel('Relative intensity (ADU)')
+        plt.show()
+
+class StarTrail():
+    pass
+
 # Open FITS file
 
 print "Read FITS file"
@@ -15,59 +68,54 @@ imgData = hdulist[0].data
 
 # Read star trail's intensity
 
+fwhms = np.array([])
+
+"""
 xmin = 2070
 xmax = 2110
-y = 3222
-plotx = []
-ploty = []
-for x in range(xmin, xmax):
-    plotx.append(x)
-    ploty.append(imgData[y, x])
-    print "Intensity of the pixel %d,%d : %d" % (x, y, imgData[y, x])
+ymin = 3212
+ymax = 3700
+"""
 
-# Convert lists to numpy arrays
+xmin = 2035
+xmax = 2070
+ymin = 3275
+ymax = 3750
 
-plotx = np.array(plotx)
-ploty = np.array(ploty)
+for y in range(ymin, ymax):
 
-# move the y data to the axis to allow the fitting
-# (because the fitting doesn't work if the data are not near the x axis)
-ploty -= min(ploty)
+    x_positions = np.array([])
+    intensities = np.array([])
 
-# Print data to verify the correctness of the previous code
+    for x in range(xmin, xmax):
+        x_positions = np.append(x_positions, x)
+        intensities = np.append(intensities, imgData[y, x])
 
-print "plotx = %s " % plotx
-print "ploty = %s " % ploty
+    slice = Slice(x_positions, intensities)
+    slice.fit_gaussian()
+    slice.fwhm_from_gaussian()
+    #slice.print_graph()
 
-# Fit the data using a Gaussian
+    sampling = 0.206  # arcsec by pixel
+    fwhmInArcsec = sampling * slice.fwhmInPx
 
-gaussian_init = models.Gaussian1D(amplitude=max(ploty), mean=sum(plotx)/len(plotx), stddev=3)
-fit_gaussian = fitting.LevMarLSQFitter()
-gaussian = fit_gaussian(gaussian_init, plotx, ploty)
+    #print "FWHM in pixels : %f" % slice.fwhmInPx
+    print "FWHM in arcsec : %f" % fwhmInArcsec
 
-print gaussian
-
-# FWHM calculation with the best-fit model
-
-sigma = gaussian.stddev.value
-fwhmInPx = 2 * np.sqrt(2 * np.log(2)) * sigma
-sampling = 0.206  # arcsec by pixel
-fwhmInArcsec = sampling * fwhmInPx
-
-print "Standard deviation : %f" % sigma
-print "FWHM in pixels : %f" % fwhmInPx
-print "FWHM in arcsec : %f" % fwhmInArcsec
+    fwhms = np.append(fwhms, fwhmInArcsec)
 
 # Plot the data with the best-fit model
 
-# 200 dots from plotx[0] to plotx[len(plotx)-1] to have a smooth bell curve
-finerPlotx = np.linspace(plotx[0], plotx[len(plotx)-1], 200)
+# min, max, avg FWHM
+print "FWHM min = %f " % np.min(fwhms)
+print "FWHM max = %f " % np.max(fwhms)
+print "FWHM mean = %f " % np.mean(fwhms)
+print "FWHM median = %f " % np.median(fwhms)
 
-plt.figure(1)
-plt.plot(plotx, ploty, 'ko')
-plt.plot(finerPlotx, gaussian(finerPlotx))
-plt.xlabel('Position (pixel)')
-plt.ylabel('Intensite (ADU)')
+plt.figure(2)
+plt.plot(fwhms, 'ko')
+plt.xlabel('Measure number')
+plt.ylabel('FWHM (arcsec)')
 plt.show()
 
 # Close FITS file
