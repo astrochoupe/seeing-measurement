81a0a29c09b56d1c92fcce586a2b7e6cee5f1364
Didier Walliang <didier.walliang@gmail.com>
2015-11-08 00:41:11 +0100

    Add the StarTrail class (represent a star trail)

--- measure.py
+++ measure.py
@@ -47,7 +47,7 @@ class Slice():
     def print_graph(self):
         # 200 dots from first to last position to have a smooth bell curve
         firstPosition = self.positions[0]
-        lastPosition = self.positions[len(x_positions)-1]
+        lastPosition = self.positions[len(self.positions)-1]
         finerPositions = np.linspace(firstPosition, lastPosition, 200)
 
         plt.figure(1)
@@ -58,17 +58,75 @@ class Slice():
         plt.show()
 
 class StarTrail():
-    pass
+    """A star trail"""
+
+    def __init__(self, xmin, xmax, ymin, ymax, imgData):
+        self.xmin = xmin
+        self.xmax = xmax
+        self.ymin = ymin
+        self.ymax = ymax
+        self.imgData = imgData
+        self.fwhms = np.array([])
+
+    def calculate_fwhms(self):
+        """Calcultate the FWHMs along the star trail"""
+
+        self.fwhms = np.array([])
+
+        for y in range(self.ymin, self.ymax):
+
+            x_positions = np.array([])
+            intensities = np.array([])
+
+            for x in range(self.xmin, self.xmax):
+                x_positions = np.append(x_positions, x)
+                intensities = np.append(intensities, self.imgData[y, x])
+
+            slice = Slice(x_positions, intensities)
+            slice.fit_gaussian()
+            slice.fwhm_from_gaussian()
+            #slice.print_graph()
+
+            sampling = 0.206  # arcsec by pixel
+            fwhmInArcsec = sampling * slice.fwhmInPx
+
+            #print "FWHM in pixels : %f" % slice.fwhmInPx
+            #print "FWHM in arcsec : %f" % fwhmInArcsec
+
+            self.fwhms = np.append(self.fwhms, fwhmInArcsec)
+
+        self.fwhm_samples = self.fwhms.size
+        self.fwhm_min = np.min(self.fwhms)
+        self.fwhm_max = np.max(self.fwhms)
+        self.fwhm_mean = np.mean(self.fwhms)
+        self.fwhm_median = np.median(self.fwhms)
+        self.fwhm_sdt_dev = np.std(self.fwhms)
+
+    def print_results(self):
+        """Print the min, max, mean, median and standard deviation of the FWHMs measurement"""
+
+        print "Samples = %i " % self.fwhm_samples
+        print "FWHM min = %f " % self.fwhm_min
+        print "FWHM max = %f " % self.fwhm_max
+        print "FWHM mean = %f " % self.fwhm_mean
+        print "FWHM median = %f " % self.fwhm_median
+        print "FWHM standard deviation = %f " % self.fwhm_sdt_dev
+
+    def print_graph(self):
+        """Plot the FWHM with the best-fit model along the star trail"""
+
+        plt.figure(1)
+        plt.plot(self.fwhms, 'ko')
+        plt.xlabel('Measure number')
+        plt.ylabel('FWHM (arcsec)')
+        plt.show()
+
 
 # Open FITS file
 
 print "Read FITS file"
 hdulist = fits.open('/home/didier/Bureau/zenith-1.fits')
 imgData = hdulist[0].data
-
-# Read star trail's intensity
-
-fwhms = np.array([])
 
 """
 xmin = 2070
@@ -82,41 +140,10 @@ xmax = 2070
 ymin = 3275
 ymax = 3750
 
-for y in range(ymin, ymax):
-
-    x_positions = np.array([])
-    intensities = np.array([])
-
-    for x in range(xmin, xmax):
-        x_positions = np.append(x_positions, x)
-        intensities = np.append(intensities, imgData[y, x])
-
-    slice = Slice(x_positions, intensities)
-    slice.fit_gaussian()
-    slice.fwhm_from_gaussian()
-    #slice.print_graph()
-
-    sampling = 0.206  # arcsec by pixel
-    fwhmInArcsec = sampling * slice.fwhmInPx
-
-    #print "FWHM in pixels : %f" % slice.fwhmInPx
-    print "FWHM in arcsec : %f" % fwhmInArcsec
-
-    fwhms = np.append(fwhms, fwhmInArcsec)
-
-# Plot the data with the best-fit model
-
-# min, max, avg FWHM
-print "FWHM min = %f " % np.min(fwhms)
-print "FWHM max = %f " % np.max(fwhms)
-print "FWHM mean = %f " % np.mean(fwhms)
-print "FWHM median = %f " % np.median(fwhms)
-
-plt.figure(2)
-plt.plot(fwhms, 'ko')
-plt.xlabel('Measure number')
-plt.ylabel('FWHM (arcsec)')
-plt.show()
+startrail = StarTrail(xmin, xmax, ymin, ymax, imgData)
+startrail.calculate_fwhms()
+startrail.print_results()
+startrail.print_graph()
 
 # Close FITS file
 
